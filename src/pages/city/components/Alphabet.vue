2cb4f2f5155eefbdf6b402e05b56e6e4ab6327fa
lengfeng <lengfeng@suneee.com>
2018-08-15 22:38:52 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="list">
+    <li class="item">a</li>
+    <li class="item">b</li>
+    <li class="item">c</li>
+    <li class="item">d</li>
+    <li class="item"></li>
+    <li class="item"></li>
+    <li class="item"></li>
+    <li class="item"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item
+      line-height .4rem
+      text-align center
+      color $bgcolor
+</style>
